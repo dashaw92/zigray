976d878e6c594a6c1aaf8a0bf342e3a4beab0ed4
dashaw92 <dashaw92@gmail.com>
2025-01-17 19:25:46 -0500

    Wrote minesweeper to learn Zig

--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,13 @@
 const std = @import("std");
 const r = @cImport(@cInclude("raylib.h"));
+const mines = @import("minesweeper.zig");
 
 pub fn main() !void {
-    r.InitWindow(640, 480, "Test");
+    var game = mines.Minesweeper(9, 9).init();
+    const win_width: c_int = @intCast((game.width - 1) * 40 + 70);
+    const win_height: c_int = @intCast((game.height - 1) * 40 + 70);
+
+    r.InitWindow(win_width, win_height, "Minesweeper");
     r.SetTargetFPS(144);
     defer r.CloseWindow();
 
@@ -10,6 +15,112 @@ pub fn main() !void {
         r.BeginDrawing();
         defer r.EndDrawing();
 
-        r.ClearBackground(r.RED);
+        r.ClearBackground(r.ORANGE);
+        switch (try interactGame(&game)) {
+            .hit_mine => game.reset(),
+            else => {},
+        }
+        renderGame(&game);
     }
+}
+
+fn rect(x: i32, y: i32, w: i32, h: i32, stroke_width: i32, stroke: r.Color, fill: r.Color) void {
+    r.DrawRectangle(x - stroke_width, y - stroke_width, w + (stroke_width * 2), h + (stroke_width * 2), stroke);
+    r.DrawRectangle(x, y, w, h, fill);
+}
+
+fn renderGame(game: anytype) void {
+    rect(5, 5, r.GetScreenWidth() - 10, r.GetScreenHeight() - 10, 3, r.BLACK, r.DARKGRAY);
+    for (0..game.height) |y| {
+        for (0..game.width) |x| {
+            const localX = 20 + 40 * x;
+            const localY = 20 + 40 * y;
+            const cell = game.cells[y * game.width + x];
+
+            if (cell.isFlagged()) {
+                renderButton(localX, localY, cell.isHidden());
+                drawText("?", localX + 10, localY + 5, r.RED);
+                continue;
+            }
+
+            switch (cell) {
+                .mine => renderButton(localX, localY, cell.isHidden()),
+                .empty => |e| {
+                    renderButton(localX, localY, e.hidden);
+                    if (!e.hidden) {
+                        drawText(nearbyToStr(e.nearby), localX + 10, localY + 5, nearbyToColor(e.nearby));
+                    }
+                },
+            }
+        }
+    }
+}
+
+fn drawText(text: [*]const u8, x: usize, y: usize, color: r.Color) void {
+    r.DrawText(text, @intCast(x + 2), @intCast(y + 2), 24, r.BLACK);
+    r.DrawText(text, @intCast(x), @intCast(y), 24, color);
+}
+
+fn renderButton(x: usize, y: usize, hidden: bool) void {
+    const mouseX = r.GetMouseX();
+    const mouseY = r.GetMouseY();
+    const is_mouseover = mouseX >= x and mouseX <= x + 30 and mouseY >= y and mouseY <= y + 30;
+
+    const color = if (!hidden) r.BROWN else if (is_mouseover) r.BEIGE else r.GRAY;
+    rect(@intCast(x), @intCast(y), 30, 30, 2, r.BLACK, color);
+}
+
+fn interactGame(game: anytype) !mines.MoveResult {
+    const mX = r.GetMouseX();
+    const mY = r.GetMouseY();
+    const left = r.IsMouseButtonPressed(r.MOUSE_BUTTON_LEFT);
+    const right = r.IsMouseButtonPressed(r.MOUSE_BUTTON_RIGHT);
+
+    // Nothing to do if no buttons were pressed
+    if (!left and !right) return undefined;
+
+    // Constrain interaction events to the game board.
+    // The minimum here prevents an underflow with usize (<20 - 20)
+    if (mX <= 20 or mY <= 20 or mX > game.width * 40 + 30 or mY > game.height * 40 + 30) return undefined;
+
+    // Convert from screen coords to the game board
+    const gameX: usize = @intCast(@divFloor(mX - 20, 40));
+    const gameY: usize = @intCast(@divFloor(mY - 20, 40));
+
+    // If the click happened beyond the edge of the game board
+    if (mX > gameX * 40 + 51 or mY > gameY * 40 + 51) return undefined;
+
+    const pos: mines.Point = .{ .x = gameX, .y = gameY };
+    const move = if (left) mines.Move{ .expose = pos } else mines.Move{ .flag = pos };
+    return game.play(move);
+}
+
+fn nearbyToStr(nearby: usize) [*]const u8 {
+    return switch (nearby) {
+        0 => "",
+        1 => "1",
+        2 => "2",
+        3 => "3",
+        4 => "4",
+        5 => "5",
+        6 => "6",
+        7 => "7",
+        8 => "8",
+        else => unreachable,
+    };
+}
+
+fn nearbyToColor(nearby: usize) r.Color {
+    return switch (nearby) {
+        0 => r.WHITE,
+        1 => r.SKYBLUE,
+        2 => r.GREEN,
+        3 => r.ORANGE,
+        4 => r.DARKBLUE,
+        5 => r.RED,
+        6 => r.MAGENTA,
+        7 => r.PURPLE,
+        8 => r.BLACK,
+        else => unreachable,
+    };
 }
---
+++ src/minesweeper.zig
@@ -0,0 +1,260 @@
+const std = @import("std");
+const Random = std.Random;
+const Fifo = std.fifo.LinearFifo;
+
+pub fn Minesweeper(comptime _width: comptime_int, comptime _height: comptime_int) type {
+    const Game = struct {
+        const Self = @This();
+
+        width: usize,
+        height: usize,
+        cells: [_width * _height]Cell = undefined,
+        lost: bool = false,
+        minesPlaced: bool = false,
+        rng: Random,
+
+        pub fn init() Self {
+            var xor = Random.DefaultPrng.init(@intCast(std.time.timestamp()));
+            const rng = xor.random();
+
+            var self: Self = .{
+                .width = _width,
+                .height = _height,
+                .rng = rng,
+            };
+
+            inline for (self.cells, 0..) |_, i| {
+                self.cells[i] = Cell{ .empty = .{} };
+            }
+
+            return self;
+        }
+
+        pub fn reset(self: *Self) void {
+            inline for (self.cells, 0..) |_, i| {
+                self.cells[i] = Cell{ .empty = .{} };
+            }
+
+            self.minesPlaced = false;
+            self.lost = false;
+        }
+
+        fn inBounds(self: *Self, pt: *const Point) bool {
+            return pt.x >= 0 and pt.x < self.width and pt.y >= 0 and pt.y < self.height;
+        }
+
+        fn neighbors(x: usize, y: usize) [8]?Point {
+            return [8]?Point{
+                if (x == 0 or y == 0) null else .{ .x = x - 1, .y = y - 1 },
+                .{ .x = x + 1, .y = y + 1 },
+                if (x == 0) null else .{ .x = x - 1, .y = y + 1 },
+                if (y == 0) null else .{ .x = x + 1, .y = y - 1 },
+                if (y == 0) null else .{ .x = x, .y = y - 1 },
+                .{ .x = x, .y = y + 1 },
+                if (x == 0) null else .{ .x = x - 1, .y = y },
+                .{ .x = x + 1, .y = y },
+            };
+        }
+
+        fn placeMines(self: *Self, avoid_x: usize, avoid_y: usize) void {
+            const needed_mines = self.height + (self.width / 2);
+            var num_mines: u8 = 0;
+            var i: usize = 0;
+            while (num_mines < needed_mines) : (i += 1) {
+                if (i >= (self.width * self.height)) i = 0;
+
+                const x = i % self.width;
+                const y = i / self.width;
+
+                // Do not place a mine at the specific coords- this position
+                // is the starting move, and placing a mine here would be
+                // really annoying for the player.
+                if (x == avoid_x and y == avoid_y) continue;
+                // Already a mine here
+                if (!self.cells[i].isEmpty()) continue;
+                // Give a large chance to not place a mine in an attempt to distribute them
+                // evenly.
+                if (self.rng.float(f32) <= 0.9) continue;
+
+                num_mines += 1;
+                self.cells[i] = Cell{ .mine = false };
+            }
+
+            //Update nearby mine count for all empty cells.
+            for (self.cells, 0..) |_, j| {
+                const x = j % self.width;
+                const y = j / self.width;
+                var cell = &self.cells[j];
+                //If we're on a mine, obviously nothing to do
+                if (!cell.isEmpty()) continue;
+
+                const neighborsOf = neighbors(x, y);
+
+                var nearby: usize = 0;
+                for (neighborsOf) |point| {
+                    if (point == null) continue;
+                    if (!self.inBounds(&point.?)) continue;
+                    const neighbor = self.cells[point.?.y * self.width + point.?.x];
+                    if (!neighbor.isEmpty()) nearby += 1;
+                }
+
+                switch (cell.*) {
+                    .empty => |*e| e.nearby = nearby,
+                    else => unreachable,
+                }
+            }
+        }
+
+        pub fn play(self: *Self, move: Move) !MoveResult {
+            if (!self.inBounds(&move.position())) {
+                return MoveResult{ .out_of_bounds = move.position() };
+            }
+            const x = move.position().x;
+            const y = move.position().y;
+            const cell: *Cell = &self.cells[y * self.width + x];
+            switch (move) {
+                .flag => {
+                    switch (cell.*) {
+                        .mine => |*mine| mine.* = !mine.*,
+                        .empty => |*empty| empty.flagged = !empty.flagged,
+                    }
+                    return .success;
+                },
+                .expose => {
+                    switch (cell.*) {
+                        .mine => {
+                            self.lost = true;
+                            return MoveResult{ .hit_mine = move.position() };
+                        },
+                        .empty => |*empty| {
+                            empty.hidden = false;
+                            if (!self.minesPlaced) {
+                                self.placeMines(x, y);
+                                self.minesPlaced = true;
+                            }
+
+                            if (empty.nearby == 0) {
+                                for (&self.cells) |*c| {
+                                    c.setVisited(false);
+                                }
+
+                                // floodfill nearby cells to expose all until
+                                // all trivial cells are exposed.
+                                // TODO Why the fuck does this crash without "* 100"
+                                var queue = Fifo(Point, .{ .Static = _width * _height * 100 }).init();
+                                defer queue.deinit();
+
+                                try queue.writeItem(move.position());
+                                while (queue.count != 0) {
+                                    const pos = queue.readItem().?;
+                                    var currentCell: *Cell = &self.cells[pos.y * self.width + pos.x];
+                                    currentCell.setVisited(true);
+
+                                    if (!currentCell.isEmpty()) continue;
+                                    currentCell.setHidden(false);
+                                    if (currentCell.nearbyMines() > 0) continue;
+
+                                    for (neighbors(pos.x, pos.y)) |neighbor| {
+                                        if (neighbor == null) continue;
+                                        if (!self.inBounds(&neighbor.?)) continue;
+                                        const neighborCell = self.cells[neighbor.?.y * self.width + neighbor.?.x];
+                                        if (neighborCell.isVisited()) continue;
+
+                                        try queue.writeItem(neighbor.?);
+                                    }
+                                }
+                            }
+                        },
+                    }
+                    return .success;
+                },
+            }
+        }
+    };
+
+    return Game;
+}
+
+pub const Cell = union(enum) {
+    mine: bool,
+    empty: EmptyCell,
+
+    pub fn setHidden(cell: *Cell, h: bool) void {
+        switch (cell.*) {
+            .mine => return,
+            .empty => |*e| e.hidden = h,
+        }
+    }
+
+    pub fn isVisited(cell: *const Cell) bool {
+        return switch (cell.*) {
+            .mine => true,
+            .empty => |e| e.visited,
+        };
+    }
+
+    pub fn setVisited(cell: *Cell, v: bool) void {
+        switch (cell.*) {
+            .mine => return,
+            .empty => |*e| e.visited = v,
+        }
+    }
+
+    pub fn isFlagged(cell: *const Cell) bool {
+        return switch (cell.*) {
+            .mine => |m| m,
+            .empty => |e| e.flagged,
+        };
+    }
+
+    pub fn isEmpty(cell: *const Cell) bool {
+        return switch (cell.*) {
+            .mine => false,
+            .empty => true,
+        };
+    }
+
+    pub fn isHidden(cell: *const Cell) bool {
+        return switch (cell.*) {
+            .mine => true,
+            .empty => |empty| empty.hidden,
+        };
+    }
+
+    pub fn nearbyMines(cell: *const Cell) usize {
+        return switch (cell.*) {
+            .mine => 0,
+            .empty => |empty| empty.nearby,
+        };
+    }
+};
+
+pub const EmptyCell = struct {
+    flagged: bool = false,
+    hidden: bool = true,
+    visited: bool = false,
+    nearby: usize = 0,
+};
+
+pub const Point = struct {
+    x: usize,
+    y: usize,
+};
+
+pub const Move = union(enum) {
+    flag: Point,
+    expose: Point,
+
+    pub fn position(move: *const Move) Point {
+        return switch (move.*) {
+            .flag => |f| f,
+            .expose => |e| e,
+        };
+    }
+};
+
+pub const MoveResult = union(enum) {
+    success,
+    out_of_bounds: Point,
+    hit_mine: Point,
+};
